95be5a69d4fda47fd3bd6177b417b33bad72842b
Travis CI User <travis@example.org>
2022-05-31 11:13:52 +0000

    Merge pull request #434 from karelhala/add-github-option
    Add github option to fetch config from GH

---
+++ js/7462.1653995555981.3f044e815d932d3b45ab.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinsights_frontend_starter_app=self.webpackChunkinsights_frontend_starter_app||[]).push([[7462],{87462:(t,r,n)=>{function e(){return e=Object.assign||function(t){for(var r=1;r<arguments.length;r++){var n=arguments[r];for(var e in n)Object.prototype.hasOwnProperty.call(n,e)&&(t[e]=n[e])}return t},e.apply(this,arguments)}n.d(r,{Z:()=>e})}}]);
+//# sourceMappingURL=../sourcemaps/7462.9ce2bb7a68dada1d76b2790605b3e92c.js.map
